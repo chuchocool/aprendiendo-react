4f33e89fc3b55d705c71ee1b502d3ccf0bc79190
chuchocool <chuchocool@hotmail.com>
2024-08-12 23:17:04 -0600

    cambios migrados desde los archivos originales

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,19 +1,43 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
 export function App () {
   return (
-    <article>
-      <header>
-        <img alt="El avatar de midudev" src="https://unavatar.io/midudev"></img>
-        <div>
-          <strong>Miguel Ángel Durán</strong>
-          <span>@midudev</span>
-        </div>
-      </header>
-
-      <aside>
-        <button>
-          Seguir
-        </button>
-      </aside>
-    </article>    
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
   )
 }
